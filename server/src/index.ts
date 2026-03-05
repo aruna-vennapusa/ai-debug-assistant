eb9479ecf75a44998159836884f9bcf5fbecea39
Aruna Vennapusa <arunaprasad.b@gmail.com>
2026-03-06 07:27:52 +1000

    wip

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,14 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = "8080";
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Allow requests from your frontend
+  }),
+);
 
 app.use(express.json());
 
